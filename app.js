ea241a768533b6dc8fb0fbcf87baf008fb9be51e
d1000m3000-sys <d1000m3000@gmail.com>
2026-05-25 21:50:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Acoin PWA running...");
+console.log("Acoin PWA Running");
+
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = "block";
+});
+
+installBtn.addEventListener("click", async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const choice = await deferredPrompt.userChoice;
+    console.log(choice.outcome);
+    deferredPrompt = null;
+  }
+});
